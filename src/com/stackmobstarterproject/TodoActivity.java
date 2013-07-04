797bfbab720be271de65b92a93262237819a9256
ardok <ardo@stackmob.com>
2013-07-03 17:14:28 -0700

    Make public key a placeholder

--- src/com/stackmobstarterproject/TodoActivity.java
+++ src/com/stackmobstarterproject/TodoActivity.java
@@ -66,7 +66,7 @@ public class TodoActivity extends Activity {
     setContentView(R.layout.activity_todo);
 
     // initialize StackMob --- Change YOUR_PUBLIC_KEY with your app's public key
-    StackMobAndroid.init(getApplicationContext(), 0, "cff876ce-f111-410e-b35c-ab22abeac7d7");
+    StackMobAndroid.init(getApplicationContext(), 0, "YOUR_PUBLIC_KEY");
 
     mTodoListView = (ListView) this.findViewById(R.id.atTodoListView);
 
